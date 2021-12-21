62c50390963a580ee0cb53494154aa213d5c35bb
WinKill <85948036+WinFuck@users.noreply.github.com>
2021-12-21 14:02:07 +0100

    Add files via upload

---
+++ WRER4.01/WRER4.01/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace WRER4._01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.wRERToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informacjeOProgramieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informacjeOWersjiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wRERToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(679, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // wRERToolStripMenuItem
+            // 
+            this.wRERToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.informacjeOProgramieToolStripMenuItem,
+            this.informacjeOWersjiToolStripMenuItem});
+            this.wRERToolStripMenuItem.Name = "wRERToolStripMenuItem";
+            this.wRERToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.wRERToolStripMenuItem.Text = "WRER...";
+            // 
+            // informacjeOProgramieToolStripMenuItem
+            // 
+            this.informacjeOProgramieToolStripMenuItem.Name = "informacjeOProgramieToolStripMenuItem";
+            this.informacjeOProgramieToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.informacjeOProgramieToolStripMenuItem.Text = "Informacje o programie";
+            this.informacjeOProgramieToolStripMenuItem.Click += new System.EventHandler(this.informacjeOProgramieToolStripMenuItem_Click);
+            // 
+            // informacjeOWersjiToolStripMenuItem
+            // 
+            this.informacjeOWersjiToolStripMenuItem.Name = "informacjeOWersjiToolStripMenuItem";
+            this.informacjeOWersjiToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.informacjeOWersjiToolStripMenuItem.Text = "Informacje o wersji";
+            this.informacjeOWersjiToolStripMenuItem.Click += new System.EventHandler(this.informacjeOWersjiToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(5, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 24);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Usun pliki .tmp...";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Cyan;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(12, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 27);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Resetuj UAC...";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Cyan;
+            this.button3.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(163, 116);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 27);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Resetuj powłokę...";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Cyan;
+            this.button4.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(134, 149);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(116, 27);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "CLICK ME";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Cyan;
+            this.button5.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.Black;
+            this.button5.Location = new System.Drawing.Point(314, 116);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(327, 27);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Ustaw defaultową tapete systemu Windows...";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Cyan;
+            this.button6.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.Black;
+            this.button6.Location = new System.Drawing.Point(12, 116);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(145, 27);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Resetuj Userinit...";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Cyan;
+            this.button7.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.Black;
+            this.button7.Location = new System.Drawing.Point(256, 149);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(217, 27);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Resetuj przyciski myszy...";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Cyan;
+            this.button8.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.Color.Black;
+            this.button8.Location = new System.Drawing.Point(479, 149);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(175, 27);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Resetuj DisableCAD...";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Cyan;
+            this.button9.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.Color.Black;
+            this.button9.Location = new System.Drawing.Point(193, 182);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(221, 27);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "Resetuj AutoAdminLogon...";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Cyan;
+            this.button10.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.Color.Black;
+            this.button10.Location = new System.Drawing.Point(12, 182);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(175, 27);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "Wlacz wiersz poleceń...";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Cyan;
+            this.button11.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.ForeColor = System.Drawing.Color.Black;
+            this.button11.Location = new System.Drawing.Point(420, 182);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(177, 27);
+            this.button11.TabIndex = 13;
+            this.button11.Text = "Wlacz edytora rejestru";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(206, 512);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 15);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "<----- tutaj jest godzina i data";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(0, 512);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(679, 529);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Windows Registry Entries Reseter v4.01";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem wRERToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informacjeOProgramieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informacjeOWersjiToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ WRER4.01/WRER4.01/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.IO;
+
+namespace WRER4._01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var NewForm = new Premium();
+            NewForm.ShowDialog();
+        }
+
+        private void informacjeOProgramieToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("To jest juz czwarta wersja programu Windows Registry Entries Reseter !", "WRER v4.01", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void informacjeOWersjiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Wersja: 4.01", "WRER v4.01", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegistryKey reg = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            reg.SetValue("EnableLUA", "1", RegistryValueKind.String);
+
+            MessageBox.Show("Ustawienia zostaly zresetowane.", "WRER v4.01", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            RegistryKey mk = Registry.CurrentUser.CreateSubKey("Control Panel\\Desktop");
+            mk.SetValue("Shell", "explorer", RegistryValueKind.String);
+            MessageBox.Show("Ustawienia zostaly zresetowane.", "WRER v4.01", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            RegistryKey mk = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Policies\\Winlogon");
+            mk.SetValue("WallPaper", "C:\\Windows\\Web\\Wallpaper\\Windows\\img0.jpg", RegistryValueKind.String);
+            MessageBox.Show("Ustawienia zostaly zresetowane.", "WRER v4.01", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("DaVinci, ogólnie dziękuje za testowanie mojego WRERa wersję 4.01! Pozdrawia Windows_Locker :) - (A i pozdrawiam rederka jeśli to oglada za pseudonim Zpizgany 'Pizduś' Jerry XD - A i pozdrawiam widzow :)", "WRER v4.01", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            RegistryKey mk1 = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Policies\\Winlogon");
+            mk1.SetValue("Userinit", "C:\\Windows\\system32\\userinit.exe,", RegistryValueKind.String);
+            MessageBox.Show("Ustawienia zostaly zresetowane.", "WRER v4.01", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            RegistryKey regrest3 = Registry.CurrentUser.CreateSubKey("Control Panel\\Mouse");
+            regrest3.SetValue("SwapMouseButtons", "0", RegistryValueKind.String);
+            MessageBox.Show("Ustawienia zostaly zresetowane.", "WRER v4.01", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            RegistryKey regrest = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Policies\\Winlogon");
+            regrest.SetValue("DisableCAD", "1", RegistryValueKind.String);
+            MessageBox.Show("Ustawienia zostaly zresetowane.", "WRER v4.01", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            RegistryKey regrest1 = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Policies\\Winlogon");
+            regrest1.SetValue("AutoAdminLogon", "1", RegistryValueKind.String);
+            MessageBox.Show("Ustawienia zostaly zresetowane.", "WRER v4.01", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            RegistryKey wrer = Registry.CurrentUser.CreateSubKey("Software\\Policies\\Microsoft\\Windows\\System");
+            wrer.SetValue("DisableCMD", "", RegistryValueKind.String);
+            MessageBox.Show("Ustawienia zostaly zresetowane.", "WRER v4.01", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            RegistryKey rk = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            rk.SetValue("DisableRegistryTools", "", RegistryValueKind.String);
+            MessageBox.Show("Ustawienia zostaly zresetowane.", "WRER v4.01", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
